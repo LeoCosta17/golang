f68446241eaee66e061fe551581f561308ae93c8
LeoCosta17 <leoocosta1209@gmail.com>
2025-11-13 22:46:33 -0300

    Carregar contatos na tela Novas funções contato

--- CRUD/controllers/controllers.go
+++ CRUD/controllers/controllers.go
@@ -12,13 +12,25 @@ func init() {
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "index.html", nil)
+func HomeHandler(w http.ResponseWriter, r *http.Request) { // Carrega a página inicial
+	contatos, err := models.BuscarContatos()
+
+	if err != nil {
+		erro := models.CustomError{
+			Mensagem: "Erro ao buscar contatos cadastrados",
+			Erro:     err,
+		}
+		templates.ExecuteTemplate(w, "erro.html", erro)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	templates.ExecuteTemplate(w, "index.html", contatos)
 }
 
-func CriarContatoHandler(w http.ResponseWriter, r *http.Request) {
+func CriarContatoHandler(w http.ResponseWriter, r *http.Request) { // Cria um novo contato
 	if r.Method != http.MethodPost {
-
 		erro := models.CustomError{
 			Mensagem: "Método não permitido",
 			Erro:     nil,
@@ -54,4 +66,32 @@ func CriarContatoHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 
 	http.Redirect(w, r, "/home", http.StatusSeeOther)
+}
+
+func BuscarContatosHandler(w http.ResponseWriter, r *http.Request) ([]models.Contato, error) {
+	contatos, err := models.BuscarContatos()
+	if err != nil {
+		erro := models.CustomError{
+			Mensagem: "Erro ao buscar contatos cadastrados",
+			Erro:     err,
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		templates.ExecuteTemplate(w, "erro.html", erro)
+		return nil, err
+	}
+
+	w.WriteHeader(http.StatusOK)
+	return contatos, nil
+}
+
+func BuscarContatoHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func EditarContatoHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func ExcluirContatoHandler(w http.ResponseWriter, r *http.Request) {
+
 }
--- CRUD/models/contato.go
+++ CRUD/models/contato.go
@@ -6,12 +6,13 @@ import (
 )
 
 type Contato struct {
+	ID       int
 	Nome     string
 	Email    string
 	Telefone string
 }
 
-func (Contato) CriarContato(contato *Contato) error {
+func (Contato) CriarContato(contato *Contato) error { // Cadastra um contato no banco de dados
 
 	db, err := database.ConnDatabase()
 	if err != nil {
@@ -28,12 +29,12 @@ func (Contato) CriarContato(contato *Contato) error {
 
 	defer statement.Close()
 
-	resultado, err := statement.Exec(contato.Nome, contato.Email, contato.Telefone)
+	resultados, err := statement.Exec(contato.Nome, contato.Email, contato.Telefone)
 	if err != nil {
 		return err
 	}
 
-	_, err = resultado.LastInsertId()
+	_, err = resultados.LastInsertId()
 	if err != nil {
 		return err
 	}
@@ -42,16 +43,44 @@ func (Contato) CriarContato(contato *Contato) error {
 	return nil
 }
 
-func (Contato) BuscarContatos() ([]Contato, error) {
+func BuscarContatos() ([]Contato, error) { // Busca no banco todos os contatos cadastrados e retorna um slice contendo todos eles
 	db, err := database.ConnDatabase()
 	if err != nil {
 		return nil, err
 	}
 	defer db.Close()
 
+	resultados, err := db.Query("SELECT * FROM usuarios")
+	if err != nil {
+		return nil, err
+	}
+
+	defer resultados.Close()
+
+	var contatos []Contato
+
+	for resultados.Next() {
+		var contato Contato
+
+		if err := resultados.Scan(&contato.ID, &contato.Nome, &contato.Email, &contato.Telefone); err != nil {
+			return nil, err
+		}
+
+		contatos = append(contatos, contato)
+	}
+
+	return contatos, nil
+}
+
+func BuscarContato() (*Contato, error) { // Busca por um contato específico no banco de dados e retorna o mesmo
 	return nil, nil
 }
 
-func (Contato) BuscarContato() (*Contato, error) {
-	return nil, nil
+func (Contato) EditarContato(contato *Contato) error { // Edita um contato específico no banco de dados
+	return nil
+}
+
+func ExcluirContato(ID_contato int) error { // Exclui um contato específico no banco de dados
+
+	return nil
 }
