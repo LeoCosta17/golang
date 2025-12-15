ed3ca402ebdecb3a2e7d2d65a21a699fb0a3ec85
LeoCosta17 <leoocosta1209@gmail.com>
2025-12-15 19:19:57 -0300

    Criado o modelo de publicação e as rotas de publicação

---
+++ Projeto Devbook/api/src/controllers/publicacoes.go
@@ -0,0 +1,60 @@
+package controllers
+
+import (
+	"api/src/autenticacao"
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"net/http"
+)
+
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, err := autenticacao.ExtrairUsuarioID(r)
+	if err != nil {
+		respostas.Erro(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	var publicacao models.Publicacao
+
+	if err := json.NewDecoder(r.Body).Decode(&publicacao); err != nil {
+		respostas.Erro(w, http.StatusBadRequest, err)
+		return
+	}
+
+	publicacao.AutorID = usuarioID
+
+	db, err := database.DBConn()
+	if err != nil {
+		respostas.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioPublicacoes(db)
+	publicacao.ID, err = repositorio.Criar(publicacao)
+	if err != nil {
+		respostas.Erro(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, publicacao)
+}
+
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
--- Projeto Devbook/api/src/router/router.go
+++ Projeto Devbook/api/src/router/router.go
@@ -8,22 +8,30 @@ import (
 
 // Retorna um roteador com todas as rotas da api
 func CreateRouter() *http.ServeMux {
+
 	router := http.NewServeMux()
 
 	router.HandleFunc("POST /login", controllers.Login)
-	router.HandleFunc("POST /users", middlewares.Logger(controllers.CriarUsuario))
+	router.HandleFunc("POST /usuarios", middlewares.Logger(controllers.CriarUsuario))
 
-	router.HandleFunc("GET /users/search/{user}", middlewares.Logger(middlewares.Autenticar(controllers.BuscarUsuariosPorIdentificador)))
-	router.HandleFunc("GET /users/search-id/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.BuscarUsuarioPorID)))
+	router.HandleFunc("GET /usuarios/buscar/{user}", middlewares.Logger(middlewares.Autenticar(controllers.BuscarUsuariosPorIdentificador)))
+	router.HandleFunc("GET /usuarios/buscar-id/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.BuscarUsuarioPorID)))
 
-	router.HandleFunc("PUT /users/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.AtualizarUsuario)))
-	router.HandleFunc("DELETE /users/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.DeleteUsuario)))
+	router.HandleFunc("PUT /usuarios/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.AtualizarUsuario)))
+	router.HandleFunc("DELETE /usuarios/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.DeleteUsuario)))
 
-	router.HandleFunc("POST /users/follow/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.SeguirUsuario)))
-	router.HandleFunc("POST /users/unfollow/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.PararSeguirUsuario)))
-	router.HandleFunc("GET /users/followers/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.BuscarSeguidores)))
-	router.HandleFunc("GET /users/following/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.Seguindo)))
+	router.HandleFunc("POST /usuarios/seguir/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.SeguirUsuario)))
+	router.HandleFunc("POST /usuarios/deixar-seguir/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.PararSeguirUsuario)))
+	router.HandleFunc("GET /usuarios/seguidores/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.BuscarSeguidores)))
+	router.HandleFunc("GET /usuarios/seguindo/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.Seguindo)))
 
-	router.HandleFunc("POST /users/update-password/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.AtualizarSenha)))
+	router.HandleFunc("POST /usuarios/atualizar-senha/{user_id}", middlewares.Logger(middlewares.Autenticar(controllers.AtualizarSenha)))
+
+	router.HandleFunc("POST /publicacoes", middlewares.Logger(middlewares.Autenticar(controllers.CriarPublicacao)))
+	router.HandleFunc("GET /publicacoes", middlewares.Logger(middlewares.Autenticar(controllers.BuscarPublicacoes)))
+	router.HandleFunc("GET /publicacoes/buscar/{publicacao_id}", middlewares.Logger(middlewares.Autenticar(controllers.BuscarPublicacao)))
+	router.HandleFunc("PUT /publicacoes/atualizar/{publicacao_id}", middlewares.Logger(middlewares.Autenticar(controllers.AtualizarPublicacao)))
+	router.HandleFunc("DELETE /publicacoes/deletar/{publicacao_id}", middlewares.Logger(middlewares.Autenticar(controllers.DeletarPublicacao)))
+
 	return router
 }
